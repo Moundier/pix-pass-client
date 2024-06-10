61f59cffb61256998eeeb4f59e6dbdcbbd64f0a9
Moundier <garamoundier.developer@protonmail.com>
2024-06-10 05:06:47 -0300

    refactor: better names for files!

---
+++ lib/shared/models/password.model.dart
@@ -0,0 +1,25 @@
+class Password {
+
+  String label; 
+  String password; // facebook1234
+
+  Password(this.label, this.password);
+
+  void insertPassword() {
+    return;
+  }
+
+  void locatePassword() {
+    return;
+  }
+
+  void updatePassword() {
+    return;
+  }
+
+  
+  void deletePassword() {
+    return;
+  }
+  
+}
---
+++ lib/shared/models/storage.model.dart
@@ -0,0 +1,26 @@
+import 'package:client_flutter/shared/models/password.model.dart';
+
+class Storage {
+  
+  String label;
+  List<Password> options;
+
+  Storage(this.label, this.options);
+
+  void insertStorage() {
+    return;
+  }
+
+  void locateStorage() {
+    return;
+  }
+
+  void updateStorage() {
+    return;
+  }
+  
+  void deleteStorage() {
+    return;
+  }
+  
+}
